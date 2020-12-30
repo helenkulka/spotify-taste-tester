dede4078756edb6b766ed4220a907b2d6f909c37
Helen Kulka <helen.kulka@mail.mcgill.ca>
2020-12-30 13:18:31 -0500

    ui changes

--- client/public/getUserData.js
+++ client/public/getUserData.js
@@ -1,12 +1,10 @@
-// import SpotifyWebApi from '../node_modules/spotify-web-api-js/src/typings/spotify-web-api';
 const spotifyApi = new SpotifyWebApi();
 
 async function getPlaylist(access_token) {
-    spotifyApi.setAccessToken(access_token)
-    playlist =  new Array(332);
+    spotifyApi.setAccessToken(access_token);
+    playlist =  new Array();
     offsets = [0, 100, 200, 300];
-    limits = [100, 100, 100, 32];
-    //console.log(spotifyApi.getPlaylistTracks("5FhPTMUBwGX8sU3qPD2stB", {offset: 0, limit: 100}))
+    limits = [100, 100, 100, 34];
     for (i =0; i < offsets.length; i++) {
         var response = await spotifyApi.getPlaylistTracks("5FhPTMUBwGX8sU3qPD2stB", {offset: offsets[i], limit: limits[i]});
         for (j =0; j < response.items.length; j++) {
@@ -15,18 +13,30 @@ async function getPlaylist(access_token) {
       }
      return await playlist;
 }
-function getLikedTracks(access_token) {
-    spotifyApi.setAccessToken(access_token)
-    spotifyApi.getPlaylistTracks("5FhPTMUBwGX8sU3qPD2stB")
-    .then((response) => {
-      console.log("blonded trax -> ", response)
-    });
+async function getLikedTracks(access_token, blonded_ids) {
+  spotifyApi.setAccessToken(access_token);
+  var contains = [];
+  console.log(blonded_ids.length)
+  for (i =0; i < 350; i+=50) {
+    var ids = blonded_ids.slice(i,i+50);
+    var res = await spotifyApi.containsMySavedTracks(ids);
+    for (j =0; j < res.length; j++) {
+      contains.push(res[j]);
+    }
+  }
+  return contains;
+
 }
+
 
 function getSavedArtists() {
 
 }
 
 function getSavedAlbums() {
+
+}
+
+function createRecommendedPlaylist() {
 
 }
