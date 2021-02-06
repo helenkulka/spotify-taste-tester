b0252ab38f14b7b8c80786edf64c549307eb5a9f
anahita-m <mohapatra.anahita@gmail.com>
2021-02-06 16:33:23 -0600

    social share buttons [WIP]

--- client/src/components/thanks.js
+++ client/src/components/thanks.js
@@ -1,6 +1,21 @@
 import React, { useRef, Component} from 'react';
 import { Container, Row, Col, Fragment, Button } from 'react-bootstrap';
 import {createRecommendedPlaylist} from './getUserData';
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    FacebookMessengerShareButton,
+    FacebookMessengerIcon,
+    RedditShareButton,
+    RedditIcon,
+    TelegramShareButton,
+    TumblrShareButton,
+    TumblrIcon,
+    TwitterShareButton,
+    TwitterIcon,
+    WhatsappShareButton,
+    WhatsappIcon,
+  } from "react-share";
 import './thanks.css'
 var recs = []
 export default class ThankYouPage extends Component {
@@ -68,19 +83,110 @@ export default class ThankYouPage extends Component {
                 {this.state.playlistLoaded ?  
                 <div id="success-save">
                     <p id="success-save-msg"> We successfully saved your playlist, enjoy! </p> 
-                    <div id="share-social">
+                    <div id="share-social c-network">
                         <p id="share-msg"> share with your friends </p>
+                        <FacebookShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    quote={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    hashtag="#frankocean">
+                    <FacebookIcon logoFillColor="white" size={"2rem"} round/>
+                </FacebookShareButton>
+                <TwitterShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <TwitterIcon logoFillColor="white" size={"2rem"} round/>
+                </TwitterShareButton>
+                <WhatsappShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <WhatsappIcon logoFillColor="white" size={"2rem"} round/>
+                </WhatsappShareButton>
+                <TumblrShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title="The Frank Ocean Metrix"
+                    caption={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <TumblrIcon logoFillColor="white" size={"2rem"} round/>
+                </TumblrShareButton>
+                <RedditShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <RedditIcon logoFillColor="white" size={"2rem"} round/>
+                </RedditShareButton>
+                <FacebookMessengerShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    appId="715761142463541"
+                    >
+                    <FacebookMessengerIcon logoFillColor="white" size={"2rem"} round/>
+                </FacebookMessengerShareButton>
                     </div>
                 </div>
                 : <p> </p>
 
                 }
             </Container> :
+            <div className="c-network">
                 <p id="success-save"> Ok, we didn't save your playlist! </p> 
-            
+
+                {/* <p >Share your results!</p> */}
+                <FacebookShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    quote={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    hashtag="#frankocean">
+                    <FacebookIcon logoFillColor="white" size={"2rem"} round/>
+                </FacebookShareButton>
+                <TwitterShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <TwitterIcon logoFillColor="white" size={"2rem"} round/>
+                </TwitterShareButton>
+                <WhatsappShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <WhatsappIcon logoFillColor="white" size={"2rem"} round/>
+                </WhatsappShareButton>
+                <TumblrShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title="The Frank Ocean Metrix"
+                    caption={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <TumblrIcon logoFillColor="white" size={"2rem"} round/>
+                </TumblrShareButton>
+                <RedditShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    >
+                    <RedditIcon logoFillColor="white" size={"2rem"} round/>
+                </RedditShareButton>
+                <FacebookMessengerShareButton
+                    className="network"
+                    url="https://www.backdrophome.com/"
+                    title={`I just took a quiz on The Frank Ocean Metric and found out I have ${this.props.overlapTracks.length} songs in common with Frank Ocean!`}
+                    appId="715761142463541"
+                    >
+                    <FacebookMessengerIcon logoFillColor="white" size={"2rem"} round/>
+                </FacebookMessengerShareButton>
+        </div>
         }
 
-        </Container>
+</Container>
         )
     }
 }
