013a3b610514e8ad5bee8d896cb00065769e10cf
Helen Kulka <helen.kulka@mail.mcgill.ca>
2021-03-19 20:57:06 -0400

    change get to post

--- routes/error.js
+++ routes/error.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    console.log("ERROR:",req.body.error);
+router.post('/', function(req, res, next) {
+    console.log("ERROR:", req.body.error);
     res.send("");
 })
 
